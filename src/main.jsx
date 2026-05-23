dd95bb3d539f619cee5cebc0214c1383d8ed1c09
Amaury-4220 <pensamientofirme223@gmail.com>
2026-05-22 20:07:20 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import SurveyAI from './App.jsx'
+import SuperAdmin from './SuperAdmin.jsx'
+import EncuestadorApp from './EncuestadorApp.jsx'
+
+const path = window.location.pathname
+const App = path.startsWith('/superadmin') ? SuperAdmin
+           : path.startsWith('/encuestador') ? EncuestadorApp
+           : SurveyAI
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><App /></React.StrictMode>
 )
